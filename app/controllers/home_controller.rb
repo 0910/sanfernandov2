d8850b538a7558562534e2dcf98e0558f2594d97
0910 <e@nuevediez.com.ar>
2016-03-17 14:42:34 -0300

    news and events prevent duplicate images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	@featured_news = News.where(status: 'published', featured: 'Yes').order(date: :desc).first(5)
   	@news = News.where(status: 'published').order(date: :desc).first(3)
-  	@events = Event.all.order(date: :desc).first(3)
+  	@events = Event.where(status: 'published').order(date: :desc).first(6)
   	@banners = Banner.where(status: 'published')
   end
 end
