2b2f9d84f8ac3c1ed4d1c3b3e035876e1951bf95
emadobao <emadobao@gmail.com>
2016-09-01 00:18:14 -0300

    add custom admin for bulletins

--- app/admin/bulletin.rb
+++ app/admin/bulletin.rb
@@ -1,17 +1,41 @@
 ActiveAdmin.register Bulletin do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  #permit_params :title, :project_type_id, :description, :benefit, :solved_problems, :image_attributes, :target_id, :category_id, :link, :city_id, :user_id
 
+  before_create do |bulletin|
+    bulletin.admin_user = current_admin_user
+  end
 
+  index do
+    column :id
+    column :date
+    column :name
+    column :admin_user
+    column 'Status', :sortable => :status do |resource|
+      column_select(resource, :status, ["published", "Pending", "Suspended"])
+    end
+    actions
+  end
+  
+  show do |p|
+    attributes_table do
+      row :date
+      row :name
+      row :file do
+      	content_tag(:span, bulletin.file.path)
+      end
+    end
+  end
+
+  form html: { multipart: true } do |f|
+    f.inputs 'Details' do
+      f.semantic_errors
+      f.input :date, as: :datepicker
+      f.input :name, :require => true
+    end
+    f.inputs "PDF File" do
+      f.input :file, :as => :file, label: 'File', hint: f.object.new_record? ? f.template.content_tag(:span, "No File Yet") : image_tag(f.object.file.url(:thumb))
+    end
+    f.actions
+  end
 end
